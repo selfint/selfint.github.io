50aea0007835473ea93de593630440254e4b4b58
Tom Selfin <selfint@gmail.com>
2025-01-02 17:23:18 +0200

    add astro boilerplate

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+
+export async function GET(context) {
+  return rss({
+    title: "Tom Selfin | Blog",
+    description: "My personal blog",
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    customData: `<language>en-us</language>`,
+  });
+}
